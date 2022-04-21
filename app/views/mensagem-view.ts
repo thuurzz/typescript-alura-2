1ba98aaf1e0900d738817d1f3cfffa02364da8eb
Arthur Vinicius <thuur.vss@gmail.com>
2022-04-21 20:01:06 -0300

    Herança e pequena surpresa

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,10 +1,7 @@
-export class MensagemView {
+import { View } from "./view";
 
-    private element: HTMLElement;
+export class MensagemView extends View {
 
-    constructor(seletor: string) {
-        this.element = document.querySelector(seletor);
-    }
 
     template(model: string): string {
         return `
@@ -14,7 +11,7 @@ export class MensagemView {
 
     update(model: string): void {
         const template = this.template(model);
-        this.element.innerHTML = template;
+        this.elemento.innerHTML = template;
     }
 
 
