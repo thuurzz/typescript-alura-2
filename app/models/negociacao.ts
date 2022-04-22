5f9ad1ea6bfb26993afbe1023c02ac731654c426
Arthur Vinicius <thuur.vss@gmail.com>
2022-04-22 11:43:24 -0300

    Protegendo nosso template

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -15,7 +15,7 @@ export class Negociacao {
         return data;
     }
 
-    public criaDe(dateString: string, quantidadeString: string, valorString: string): Negociacao {
+    public static criaDe(dateString: string, quantidadeString: string, valorString: string): Negociacao {
         const exp = /-/g;
         const date = new Date(dateString.replace(exp, ','));
         const quantidade = parseInt(quantidadeString)
